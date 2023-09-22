0cb723be1be3d88461077919e946d2b4931239b2
aakozl71 <aakozl71@mts.ru>
2023-09-22 09:49:04 +0300

    Добавил агрегатор

---
+++ src/aggregator-service.ts
@@ -0,0 +1,60 @@
+import express from "express";
+import axios from "axios";
+
+import { IRestaurant } from "./restaurant-service";
+import { ICity } from "./city-service";
+import { IHotel } from "./hotel-service";
+
+const app = express();
+
+app.get("/hotels", async (req: any, res) => {
+			try {
+				const hotels = await axios.get(`http://localhost:5001/hotels`);
+				res.json(hotels.data);
+			} catch (error) {
+				console.error("Ошибка при получении отелей", error);
+				res.status(500).json({ error: "Ошибка при получении отелей" });
+			}
+});
+
+app.get("/restaurants", async (req: any, res) => {
+	
+			try {
+				const restaurants = await axios.get(`http://localhost:5002/restaurants`);
+				res.json(
+          {
+            customers: restaurants.data
+          }
+        );
+			} catch (error) {
+				console.error("Ошибка при получении ресторанов", error);
+				res.status(500).json({ error: "Ошибка при получении ресторанов" });
+			}
+});
+
+app.get("/cities", async (req: any, res) => {
+			try {
+        const hotels: IHotel[] = (await axios.get(`http://localhost:5001/hotels`)).data;
+        const restaurants: IRestaurant[] = (await axios.get(`http://localhost:5002/restaurants`)).data;
+				const cities: ICity[] = (await axios.get(`http://localhost:5003/cities`)).data;
+
+        const result = [];
+
+        cities.forEach(city => {
+          result.push({
+            city,
+            restaurants: restaurants.filter(restaurant => restaurant.cities.some(el => el === city.cityId)),
+            hotels: hotels.filter(hotel => hotel.cities.some(el => el === city.cityId)),
+          })
+        })
+
+				res.json(result);
+			} catch (error) {
+				console.error("Ошибка при получении городов", error);
+				res.status(500).json({ error: "Ошибка при получении городов" });
+			}
+});
+
+app.listen(3001, () => {
+	console.log("Сервер запущен на порту 3000");
+});
--- src/gateway.ts
+++ src/gateway.ts
@@ -29,8 +29,8 @@ app.get("/hotels", verifyToken, (req: any, res) => {
 			res.sendStatus(403);
 		} else {
 			try {
-				const hotels = await axios.get(`http://localhost:5001/hotels`);
-				res.json(hotels.data);
+				const hotels = (await axios.get(`http://localhost:3001/hotels`)).data;
+				res.json(hotels);
 			} catch (error) {
 				console.error("Ошибка при получении отелей", error);
 				res.status(500).json({ error: "Ошибка при получении отелей" });
@@ -45,12 +45,8 @@ app.get("/restaurants", verifyToken, (req: any, res) => {
 			res.sendStatus(403);
 		} else {
 			try {
-				const restaurants = await axios.get(`http://localhost:5002/restaurants`);
-				res.json(
-          {
-            customers: restaurants.data
-          }
-        );
+				const restaurants = (await axios.get(`http://localhost:3001/restaurants`)).data;
+				res.json(restaurants);
 			} catch (error) {
 				console.error("Ошибка при получении ресторанов", error);
 				res.status(500).json({ error: "Ошибка при получении ресторанов" });
@@ -65,21 +61,8 @@ app.get("/cities", verifyToken, (req: any, res) => {
 			res.sendStatus(403);
 		} else {
 			try {
-        const hotels: IHotel[] = (await axios.get(`http://localhost:5001/hotels`)).data;
-        const restaurants: IRestaurant[] = (await axios.get(`http://localhost:5002/restaurants`)).data;
-				const cities: ICity[] = (await axios.get(`http://localhost:5003/cities`)).data;
-
-        const result = [];
-
-        cities.forEach(city => {
-          result.push({
-            city,
-            restaurants: restaurants.filter(restaurant => restaurant.cities.some(el => el === city.cityId)),
-            hotels: hotels.filter(hotel => hotel.cities.some(el => el === city.cityId)),
-          })
-        })
-
-				res.json(result);
+				const cities: ICity[] = (await axios.get(`http://localhost:3001/cities`)).data;
+				res.json(cities);
 			} catch (error) {
 				console.error("Ошибка при получении городов", error);
 				res.status(500).json({ error: "Ошибка при получении городов" });
